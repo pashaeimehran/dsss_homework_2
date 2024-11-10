876aac6c9ec5fa9d537c6aea5b009897cb077e3b
pashaeimehran <157053140+pashaeimehran@users.noreply.github.com>
2024-11-10 15:51:52 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="math_quiz",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'math_quiz=math_quiz.math_quiz:math_quiz',
+        ],
+    },
+)
 
